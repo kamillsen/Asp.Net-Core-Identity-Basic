04c2ed27e5e05b886ff8fd6945f82a9c3e6233a3
kamillsen <senkamil1864mail.com>
2024-11-07 00:51:52 +0300

    Db de olan rol ile kullanıcı eklemele yapıldı

---
+++ User.Management.Service/Services/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.Management.Service.Models;
+
+namespace User.Management.Service.Services
+{
+    internal class EmailService : IEmailService
+    {
+        private readonly EmailConfiguration _emailConfig;
+
+        public EmailService(EmailConfiguration emailConfig) => _emailConfig = emailConfig;
+
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+            Send(emailMessage);
+        }
+
+        public void SendMail(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("email", _emailConfig.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+
+            return emailMessage;
+        }
+
+    }
+}
---
+++ User.Management.Service/Services/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.Management.Service.Models;
+
+namespace User.Management.Service.Services
+{
+    internal interface IEmailService
+    {
+        void SendMail(Message message);
+    }
+}
